2f704b71de1fc05cad72b95470de475771614120
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-18 21:34:13 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,10 +1,11 @@
 class Meal
 
-  attr_accessor :total, :tip
+  attr_accessor :waiter, :customer, :total, :tip
 
   @@all = Array.new
 
   def initialize(waiter, customer, total, tip = 0)
+
     @total = total
     @tip = tip
     @@all << self
